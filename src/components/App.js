97039d3587a3873ec9d13c8e61033419f79c2464
Sarah Edison <edison.sarahc@gmail.com>
2022-05-08 11:20:17 -0400

    remove team from site

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@
 import '../App.css';
 import Header from "./Header";
 import Main from "./Main";
-import Team from "./Team";
 import Footer from "./Footer";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <div className="root">
       <Header />
       <Main />
-      <Team />
       <Footer />
     </div>
   );
