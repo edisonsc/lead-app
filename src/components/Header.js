306136dbc962a913a5fc359a7c11895160076fa8
Sarah Edison <edison.sarahc@gmail.com>
2022-05-15 11:20:44 -0400

    feat: add text style to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ function Header() {
   return (
       <header className="header">
        <div className="header__area">
- 
           <h1 className="header__text">LEAD College Consulting, LLC</h1>
           <nav></nav>
           </div>
