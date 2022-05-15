bfd80593b338373a43217f1b4473d0d91e567c28
Sarah Edison <edison.sarahc@gmail.com>
2022-05-15 12:02:13 -0400

    fix: google form link

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,7 @@ function Form() {
   return (
     <div className="section">
         
-        <a href="https://forms.gle/k42VUYqraswwufQC9" className="button" target="_blank" rel="noreferrer" >Schedule a consultation</a>
+        <a href="https://forms.gle/k42VUYqraswwufQC9" className="button" target="_blank"  rel="noopener noreferrer" >Schedule a consultation</a>
 
     </div>
   );
