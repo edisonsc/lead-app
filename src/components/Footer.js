76905e2b20a5a8cd95eeac69a2c44bbdb7642b03
Sarah Edison <edison.sarahc@gmail.com>
2022-05-15 11:20:13 -0400

    remove: info email from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="footer">
           <p className="footer__text">LEAD College Consulting, LLC</p>
-          <p className="footer__text"><a href="mailto:info@leadcollegeconsulting.com">info@leadcollegeconsulting.com</a></p>
+
         </footer>
     );
   }
