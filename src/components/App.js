d41c45fa7d5e96e699e1b93a1c784d585090b364
Sarah Edison <edison.sarahc@gmail.com>
2022-05-15 11:20:02 -0400

    feat: import Form and Team components

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,17 @@
 import '../App.css';
 import Header from "./Header";
 import Main from "./Main";
+import Team from "./Team";
 import Footer from "./Footer";
+import Form from "./Form"
 
 function App() {
   return (
     <div className="root">
       <Header />
       <Main />
+      <Team />
+      <Form />
       <Footer />
     </div>
   );
