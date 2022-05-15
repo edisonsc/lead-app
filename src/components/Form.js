d262708f7088ec3e660153a8526c6be64a45c6df
Sarah Edison <edison.sarahc@gmail.com>
2022-05-15 11:20:22 -0400

    feat: create Form component

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Form() {
+  return (
+    <div className="section">
+        
+        <a href="https://forms.gle/k42VUYqraswwufQC9" className="button" target="_blank" rel="noreferrer" >Schedule a consultation</a>
+
+    </div>
+  );
+}
+
+export default Form;
